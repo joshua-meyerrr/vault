633d6d68cdf0b692465234f50417b370c6628777
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-10 18:23:56 +0200

    ✨ Create basic express server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,20 @@
-import { readCredentials } from './utils/credentials';
+import { getCredential, readCredentials } from './utils/credentials';
+import express, { request } from 'express';
 
-readCredentials();
+const app = express();
+const port = 3000;
+
+getCredential('Google');
+
+app.get('/api/credentials', async (_request, response) => {
+  const credentials = await readCredentials();
+  response.status(200).json(credentials);
+});
+
+app.get('/', (_requests, response) => {
+  response.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
