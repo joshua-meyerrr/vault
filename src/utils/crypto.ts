c198e1bc7634a820af860b63da84a6bd59603dd3
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-12 16:38:54 +0200

    ✨ Add decryption function

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -14,3 +14,17 @@ export function encryptCredential(credential: Credential): Credential {
 
   return encryptedCredential;
 }
+
+export function decryptCredential(credential: Credential): Credential {
+  const decryptedPassword = CryptoJS.TripleDES.decrypt(
+    credential.password,
+    'lmao'
+  ).toString(CryptoJS.enc.Utf8);
+
+  const decryptedCredential = {
+    ...credential,
+    password: decryptedPassword,
+  };
+
+  return decryptedCredential;
+}
