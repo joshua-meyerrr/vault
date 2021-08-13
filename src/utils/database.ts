c1fa3f0a4bd6bd616302d7579e0f2fb649eac6fe
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-13 15:07:51 +0200

    ✨  Add mongodb connection functions

---
+++ src/utils/database.ts
@@ -0,0 +1,16 @@
+import { Db, MongoClient, Collection } from 'mongodb';
+
+let client: MongoClient;
+
+export async function connectDatabase(url: string): Promise<void> {
+  client = new MongoClient(url);
+  await client.connect();
+}
+
+export function getCollection<T>(name: string): Collection<T> {
+  return client.db().collection<T>(name);
+}
+
+export function getCredentialCollection(): Collection<Credential> {
+  return getCollection<Credential>('credentials');
+}
