ca0d866bfbd6a015029dd7a72d1827ac41d1500e
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-19 16:44:24 +0200

    ✨ Implement useEffect to fetch data from backend

--- src/app/pages/Dashboard/Dashboard.tsx
+++ src/app/pages/Dashboard/Dashboard.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Dashboard.module.css';
 import { Link } from 'react-router-dom';
+import type { Credential } from '../../../types';
 
 export default function Dashboard(): JSX.Element {
+  const [credentials, setCredentials] = useState<Credential[]>([]);
+
+  useEffect(() => {
+    async function fetchCredentials() {
+      const response = await fetch('/api/credentials', {
+        headers: {
+          Authorization: 'lmao',
+        },
+      });
+      const credentials = await response.json();
+      setCredentials(credentials);
+    }
+    fetchCredentials();
+  }, []);
+
   return (
     <main className={styles.container}>
       <h1 className={styles.header}>Vault 🕋</h1>
@@ -15,6 +31,8 @@ export default function Dashboard(): JSX.Element {
       <Link className={styles.destruction} to="/selfdestruction">
         Start Self-Destruction 💣
       </Link>
+      <Link to="/passwords/google">Test Link to Google PW</Link>
+      {credentials?.forEach((credential) => console.log(credential))}
     </main>
   );
 }
