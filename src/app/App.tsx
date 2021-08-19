2cf2f607bdd86d4d165491dac649419b889836be
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-19 12:13:10 +0200

    ✨ Create Routes to passwords and destruction page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import styles from './App.module.css';
-import Dashboard from './pages/Dashboard/dashboard';
+import Dashboard from './pages/Dashboard/Dashboard';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Destruction from './pages/Destruction/Destruction';
+import Password from './pages/Passwords/Passwords';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <Dashboard />
+        </Route>
+        <Route exact path="/selfdestruction">
+          <Destruction />
+        </Route>
+        <Route path="/passwords/:service">
+          <Password />
         </Route>
       </Switch>
     </BrowserRouter>
