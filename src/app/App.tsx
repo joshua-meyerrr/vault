3cd7b1ebb644e18b79926df025b75c0c41389134
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-19 10:41:18 +0200

    ✨ Add basic BrowserRouting to App fct

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './App.module.css';
 import Dashboard from './pages/Dashboard/dashboard';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App(): JSX.Element {
   return (
-    <>
-      <Dashboard />
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
