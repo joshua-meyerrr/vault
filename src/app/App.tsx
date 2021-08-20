b06b7a541ea72e41e81795982212582bd581ad3c
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-20 17:41:43 +0200

    ✨ Add route to AddPage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import Dashboard from './pages/Dashboard/Dashboard';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Destruction from './pages/Destruction/Destruction';
 import Password from './pages/Passwords/Passwords';
+import Add from './pages/Add/Add';
 
 function App(): JSX.Element {
   return (
@@ -11,8 +12,11 @@ function App(): JSX.Element {
         <Route exact path="/">
           <Dashboard />
         </Route>
-        <Route exact path="/selfdestruction">
+        <Route path="/selfdestruction">
           <Destruction />
+        </Route>
+        <Route path="/add">
+          <Add />
         </Route>
         <Route path="/passwords/:service">
           <Password />
