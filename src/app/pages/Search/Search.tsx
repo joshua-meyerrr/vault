de2ee40b80299f04bdeebab9c47a7db340e64546
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-23 14:11:39 +0200

    💄 Add basic styling to search page

--- src/app/pages/Search/Search.tsx
+++ src/app/pages/Search/Search.tsx
@@ -4,11 +4,11 @@ import styles from './Search.module.css';
 export default function Search(): JSX.Element {
   return (
     <main className={styles.container}>
-      <h1>Vault</h1>
-      <form className={styles.container}>
+      <h1 className={styles.header}>Vault</h1>
+      <form className={styles.form}>
         <input type="text" placeholder="Service" />
         <input type="text" placeholder="Master Password" />
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Search" />
       </form>
     </main>
   );
