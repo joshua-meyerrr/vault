30d527386e8cb19becc970ad9369ddfa03ab52dc
joshua-meyerrr <joshua.meyer.personal@gmail.com>
2021-08-23 14:22:08 +0200

    ✨ Get Credential from API

--- src/app/pages/Search/Search.tsx
+++ src/app/pages/Search/Search.tsx
@@ -1,28 +1,52 @@
 import React, { useState } from 'react';
 import styles from './Search.module.css';
+import type { Credential } from '../../../types';
+import CredentialCard from '../../components/CredentialCard/CredentialCard';
+import { Link } from 'react-router-dom';
 
 export default function Search(): JSX.Element {
   const [service, setService] = useState<string>('');
   const [masterPassword, setMasterPassword] = useState<string>('');
+  const [credential, setCredential] = useState<Credential | null>(null);
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const response = await fetch(`/api/credentials/${service}`, {
+      headers: { Authorization: masterPassword },
+    });
+    if (!response.ok) {
+      console.log('Credential not found');
+      return;
+    }
+    const credential: Credential = await response.json();
+    setCredential(credential);
+  }
 
   return (
     <main className={styles.container}>
       <h1 className={styles.header}>Vault</h1>
-      <form className={styles.form}>
-        <input
-          type="text"
-          placeholder="Service"
-          value={service}
-          onChange={(event) => setService(event.target.value)}
-        />
-        <input
-          type="text"
-          placeholder="Master Password"
-          value={masterPassword}
-          onChange={(event) => setMasterPassword(event.target.value)}
-        />
-        <input type="submit" value="Search" />
-      </form>
+      {credential ? (
+        <>
+          <CredentialCard credential={credential} />
+          <Link to="/">Back to Dashboard</Link>
+        </>
+      ) : (
+        <form className={styles.form} onSubmit={(event) => handleSubmit(event)}>
+          <input
+            type="text"
+            placeholder="Service"
+            value={service}
+            onChange={(event) => setService(event.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Master Password"
+            value={masterPassword}
+            onChange={(event) => setMasterPassword(event.target.value)}
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      )}
     </main>
   );
 }
